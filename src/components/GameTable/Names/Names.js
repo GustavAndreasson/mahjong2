df46e6a1bce8be51ea8251332a1b5dfc6aa28d4b
Gustav Andreasson <gustav.andreasson@gmail.com>
2021-01-12 08:00:23 +0100

    Added pause icon when player paused

--- src/components/GameTable/Names/Names.js
+++ src/components/GameTable/Names/Names.js
@@ -4,8 +4,13 @@ import "./Names.scss";
 const Names = ({ names, pause, edit }) => (
     <div className="names">
         <div>
-            {names && names.map((name, i) => (
-                <div key={i} className={pause && pause.includes(i) ? "paused" : ""} onClick={() => edit(i)}>{name}</div>
+            { names && names.map((name, i) => (
+                <div key={i} className={pause && pause.includes(i) ? "paused" : ""} onClick={() => edit(i)}>
+                    { pause && pause.includes(i) &&
+                        <span className="fas fa-pause"></span>
+                    }
+                    { name }
+                </div>
             ))}
         </div>
     </div>
